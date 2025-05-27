11a79b0403e3c42b9aea07ee2eef6e16f238ecc8
Bob Kozdemba <bkozdemba@gmail.com>
2025-05-27 21:55:23 +0000

    feat: refactor model saving functionality and improve directory management

--- src/04-train-lr-model.py
+++ src/04-train-lr-model.py
@@ -11,47 +11,51 @@ import os
 logger = logging.getLogger()
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def concatenate_files(file_list, output_file):
+def copy_file(src_file, output_file):
     """
     Concatenate multiple files into a single output file.
     :param file_list: List of input file paths to concatenate.
     :param output_file: Output file path.
     """
     with open(output_file, 'w') as outfile:
-        for filename in file_list:
-            with open(filename, 'r') as infile:
+            with open(src_file, 'r') as infile:
                 for line in infile:
                     outfile.write(line)
 
-def create_model_repo():
+def create_model_repo(model_repository: str = "models"):
     """
     Create a model repository for Triton Inference Server.
     This function creates a directory structure for the model
     and saves the model in the appropriate format.
     """
-    model_repository = "models/lr"
-    dir_path = model_repository + "/1"
+    cwd = os.getcwd()
+    target_dir = cwd + "/pytorch/" + model_repository
 
-    if os.path.isdir(dir_path):
-        logger.info(f"Directory '{dir_path}' exists.")
+    if os.path.isdir(target_dir):
+        logger.info(f"Directory '{target_dir}' exists.")
     else:
-        logger.info(f"Directory '{dir_path}' does not exist.")
-        os.makedirs(dir_path)
-        logger.info(f"Directory '{dir_path}' created.")
+        logger.info(f"Directory '{target_dir}' does not exist.")
+        os.makedirs(target_dir, exist_ok=True)
+        logger.info(f"Directory '{target_dir}' created.")
 
-    # Save the model
-    model_file = "/model.pt"
-    model_file_path =  dir_path + model_file
-    scripted_model = torch.jit.script(our_model)
-    torch.jit.save(scripted_model, model_file_path)
-    logger.info(f"Saved PyTorch Model State to {model_file_path}")
 
+def save_model(name: str, version: int, model: torch.nn.Module):
+    """
+    Save the PyTorch model to a file.
+    :param name: Name of the model.
+    :param version: Version of the model.
+    :param model: The PyTorch model to save.
+    """
+    cwd = os.getcwd()
+    model_path = f"{cwd}/pytorch/models/{name}/{version}/model.pt"
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
+    torch.jit.save(torch.jit.script(model), model_path)
+    logger.info(f"Model saved to {model_path}")
     # Save the model protobuf config file
-    config_file = ['src/config.pbtxt']
-    config_file_path = model_repository + '/config.pbtxt'
-    concatenate_files(config_file, config_file_path)
+    config_file = cwd + '/pytorch/src/config.pbtxt'
+    config_file_path = cwd + "/pytorch/models/" + name + '/config.pbtxt'
+    copy_file(config_file, config_file_path)
     logger.info(f"Saved Triton Model Config to {config_file_path}")
-
 
 class LinearRegressionModel(torch.nn.Module):
     """
@@ -113,4 +117,5 @@ pred_y = our_model(new_var)
 logger.info(f"{new_var = } {pred_y = }")
 
 create_model_repo()
+save_model("lr", 1, our_model)
 
